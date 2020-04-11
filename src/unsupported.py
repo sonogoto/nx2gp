301e8aee7f02506fdbb30ab41cb201a7411a8362
Simon <>
2020-04-11 09:59:22 +0800

    fixed bug when decorate function with args

--- src/unsupported.py
+++ src/unsupported.py
@@ -7,9 +7,17 @@ class Unsupported(Exception):
 
 def unsupported(err_msg):
     def _unsupported(func):
-        def _raise_exception():
+        def _raise_exception(*args, **kwargs):
             raise Unsupported(err_msg)
         return _raise_exception
 
     return _unsupported
 
+
+@unsupported("test unsupported")
+def test_unsupported(arg1, arg2):
+    pass
+
+
+if __name__ == "__main__":
+    test_unsupported(1, 2)
